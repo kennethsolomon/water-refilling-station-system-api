9be511ea8fa246413ad763306d9a441bd03b317d
Kenneth Solomon <synecderz@gmail.com>
2022-06-21 20:32:38 +0800

    Sanctum and basic api route

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,47 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use Illuminate\Validation\ValidationException;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+// Register
+Route::get('register', function(Request $request){
+    $user = User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => bcrypt($request->password)
+    ]);
+
+    return $user;
+});
+
+// login
+Route::post('login', function(Request $request){
+    $credentials = $request->only('email', 'password');
+
+    if(! auth()->attempt($credentials)){
+        throw ValidationException::withMessages([
+            'email' => 'Invalid credentials'
+        ]);
+    }
+
+    $request->session()->regenerate();
+
+    return response()->json(null, 201);
+});
+
+// logout
+Route::post('logout', function(Request $request){
+    auth()->guard('web')->logout();
+
+    $request->session()->invalidate();
+
+    $request->session()->regenerateToken();
+
+    return response()->json(null, 200);
 });
