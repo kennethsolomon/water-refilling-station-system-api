cf5bbdd79d4c304cb96c3384ce08f592c1f73859
Kenneth Solomon <synecderz@gmail.com>
2022-06-22 21:45:57 +0800

    move to authentication to authcontroller

--- routes/api.php
+++ routes/api.php
@@ -1,47 +1,15 @@
 <?php
 
-use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Validation\ValidationException;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'auth:sanctum'], function(){
+    Route::get('/user', function (Request $request){
+        return $request->user();
+    });
+
+    Route::get("register",[\App\Http\Controllers\AuthController::class,'register']);
+    Route::post("logout",[\App\Http\Controllers\AuthController::class,'logout']);
 });
 
-// Register
-Route::get('register', function(Request $request){
-    $user = User::create([
-        'name' => $request->name,
-        'email' => $request->email,
-        'password' => bcrypt($request->password)
-    ]);
-
-    return $user;
-});
-
-// login
-Route::post('login', function(Request $request){
-    $credentials = $request->only('email', 'password');
-
-    if(! auth()->attempt($credentials)){
-        throw ValidationException::withMessages([
-            'email' => 'Invalid credentials'
-        ]);
-    }
-
-    $request->session()->regenerate();
-
-    return response()->json(null, 201);
-});
-
-// logout
-Route::post('logout', function(Request $request){
-    auth()->guard('web')->logout();
-
-    $request->session()->invalidate();
-
-    $request->session()->regenerateToken();
-
-    return response()->json(null, 200);
-});
+    Route::post("login",[\App\Http\Controllers\AuthController::class,'login']);
