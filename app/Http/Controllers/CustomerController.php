67b377deb4a8ce3213ecbb216bb182db49ce9ce9
Kenneth Solomon <synecderz@gmail.com>
2022-07-15 00:21:44 +0800

    remove unused import

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\CustomerPostRequest;
 use App\Models\Customer;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Symfony\Component\HttpFoundation\Response;
 
